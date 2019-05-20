7a288bb1c936a0084a66ff43c03c23eacce9fbd5
lxl <lxl@DESKTOP-14I1O47>
2019-05-20 12:31:30 +0800

    deleted methods in SubmitleaveController

--- laps/src/main/java/sg/edu/nus/demo/controller/SubmitLeaveController.java
+++ laps/src/main/java/sg/edu/nus/demo/controller/SubmitLeaveController.java
@@ -5,39 +5,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
 import org.springframework.web.servlet.ModelAndView;
 
 import sg.edu.nus.demo.model.AnnualLeave;
 import sg.edu.nus.demo.repo.AnnualLeaveRepository;
-import sg.edu.nus.demo.repo.CompLeaveRepository;
-import sg.edu.nus.demo.repo.MedicalLeaveRepository;
+
 
 @Controller
 public class SubmitLeaveController {
 
 	@Autowired
 	private AnnualLeaveRepository annualleaveRepository;
-	
-	public void setAnnualleaveRepository(AnnualLeaveRepository annualleaveRepository) {
-		this.annualleaveRepository = annualleaveRepository;
-	}
-	
-	@Autowired
-    private CompLeaveRepository comleaveRepository;
-
-	public void setComleaveRepository(CompLeaveRepository comleaveRepository) {
-		this.comleaveRepository = comleaveRepository;
-	}
-
-	@Autowired
-	private MedicalLeaveRepository medicalleaveRepository;
-	
-	public void setMedicalleaveRepository(MedicalLeaveRepository medicalleaveRepository) {
-		this.medicalleaveRepository = medicalleaveRepository;
-	}
-	
 	
 	@GetMapping("/annualLeaveForm")
 	public String createNewAnnualLeave(Model model) {
@@ -57,34 +36,6 @@ public class SubmitLeaveController {
 		model.addAttribute("annualLeave", annualLeave);
 		return "annualleaveconfirmation";
 	}
-	
-	//xiaolin update 61-86
-	@RequestMapping(path="/")
-	public String viewPersonalLeave () {		
-		return "index";
-	}
-
-	@RequestMapping(path="/viewleave", method = RequestMethod.GET)
-	public String viewLeave () {		
-		return "ViewLeave";
-	}
-	
-	@RequestMapping(path="/viewAnnualleave", method = RequestMethod.GET)
-	public String viewAnnuallLeave (Model model) {
-		model.addAttribute("annualleave",annualleaveRepository.findAll());
-		return "ViewAnnualLeave";
-	}
-	@RequestMapping(path="/viewComplLeave", method = RequestMethod.GET)
-	public String viewComplLeave (Model model) {
-		model.addAttribute("compleave",comleaveRepository.findAll());
-		return "ViewCompLeave";
-	}
-	@RequestMapping(path="/viewMedicalLeave", method = RequestMethod.GET)
-	public String viewMedicalLeave (Model model) {
-		model.addAttribute("medicalleave",medicalleaveRepository.findAll());
-		return "ViewMedicalLeave";
-	}
-	
 	
 	
 }
