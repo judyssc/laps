b1af75bb1e480e710b7ea4bd58a2907c2177b99f
Guoan Cao <e0390030@u.nus.edu>
2019-05-20 18:55:53 +0800

    enrich updateleavecontroller

--- laps/src/main/java/sg/edu/nus/demo/controller/UpdateLeaveController.java
+++ laps/src/main/java/sg/edu/nus/demo/controller/UpdateLeaveController.java
@@ -1,8 +1,34 @@
 package sg.edu.nus.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import sg.edu.nus.demo.repo.*;
+
 @Controller
-public class UpdateLeaveController {
+public class UpdateLeaveController {	
+	private EmployeeRepository employeeRepository;
+    @Autowired
+    public void setEmployeeRepository(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+       
+	private AnnualLeaveRepository annualLeaveRepository;
+    @Autowired
+    public void setAnnualLeaveRepository(AnnualLeaveRepository annualLeaveRepository) {
+        this.annualLeaveRepository = annualLeaveRepository;
+    }   
+    
+    private MedicalLeaveRepository medicalLeaveRepository;
+    @Autowired
+    public void setMedicalLeaveRepository(MedicalLeaveRepository medicalLeaveRepository) {
+        this.medicalLeaveRepository = medicalLeaveRepository;
+    }
+    
+    private CompLeaveRepository compLeaveRepository;
+    @Autowired
+    public void setCompLeaveRepository(CompLeaveRepository compLeaveRepository) {
+        this.compLeaveRepository = compLeaveRepository;
+    }
 
 }
