37c486c1a4e972b0213a172c38c05ca62b7de0b1
Shuang <qingshuang.chan@gmail.com>
2019-05-22 22:56:56 +0800

    Continuation of previous commit as some code were not pushed out

--- laps/src/main/java/sg/edu/nus/demo/controller/LoginController.java
+++ laps/src/main/java/sg/edu/nus/demo/controller/LoginController.java
@@ -1,5 +1,7 @@
 package sg.edu.nus.demo.controller;
 
+import javax.annotation.Resource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,58 +9,86 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import sg.edu.nus.demo.model.Administrator;
 import sg.edu.nus.demo.model.Employee;
+import sg.edu.nus.demo.model.Manager;
+import sg.edu.nus.demo.repo.EmployeeRepository;
 import sg.edu.nus.demo.service.LoginService;
 
 @Controller
 public class LoginController {
 	
+	
 	@Autowired
 	LoginService loginService;
 	
+	@Resource
+	private EmployeeRepository employeeRepository;
+	
+	//Employee login
 	@GetMapping("/login/employee")
-	public String login(Model model) {
+	public String loginEmployee(Model model) {
 		
 		model.addAttribute("employee", new Employee());
-	        
-		//Then show login page
-	    return "loginemployee";
+	    return "loginemployee"; //to see login page
 	}
 	
 	@PostMapping("/authenticateEmployee")
-	public String authenticateEmployee(@ModelAttribute Employee employee) {
+	public String authenticateEmployee(Model model, @ModelAttribute Employee employee) {
 		// After keying in the username and password
 		boolean loginTrueFalse = loginService.authenticateEmployee(employee.getUserId(), employee.getPassword());
 
 		if(loginTrueFalse) {
-			return "loginsuccessfulemployee";
+			Employee emp = employeeRepository.findEmployeeByUserId(employee.getUserId());
+			model.addAttribute("employee", employee);
+			return "index";
 		}
 		else {
 			return "loginemployee";
 		}
 	}
 	
-//	@GetMapping("/login")
-//	public String login(Model model) {
-//		//Prepare a User model called "user" in the form 
-//		model.addAttribute("user", new User());
-//	        
-//		//Then show login page
-//	    return "login";
-//	}
-//	
-//	@PostMapping("/authenticateEmployee")
-//	public String authenticateEmployee(@ModelAttribute User user) {
-//		//@PathVariable
-//		// After keying in the username and password
-//		boolean loginTrueFalse = loginService.authenticateEmployee(user.getUserId(), user.getPassword());
-//	        //model.addAttribute("error", "Invalid username and/or password");
-//
-//		if(loginTrueFalse) {
-//			return "loginsuccessful";
-//		}
-//		else {
-//			return "login";
-//		}
-//	}
+	//Manager login
+	@GetMapping("/login/manager")
+	public String loginManager(Model model) {
+		
+		model.addAttribute("manager", new Manager());
+	    return "loginmanager";
+	}
+	
+	@PostMapping("/authenticateManager")
+	public String authenticateManager(Model model, @ModelAttribute Manager manager) {
+		// After keying in the username and password
+		boolean loginTrueFalse = loginService.authenticateManager(manager.getUserid(), manager.getPassword());
+
+		if(loginTrueFalse) {
+			model.addAttribute("manager", manager);
+			return "loginsuccessfulmanager";
+		}
+		else {
+			return "loginmanager";
+		}
+	}
+	
+	//Administrator login
+	@GetMapping("/login/administrator")
+	public String loginAdministrator(Model model) {
+		
+		model.addAttribute("administrator", new Administrator());
+	    return "loginadministrator";
+	}
+	
+	@PostMapping("/authenticateAdministrator")
+	public String authenticateAdministator(Model model, @ModelAttribute Administrator administrator) {
+		// After keying in the username and password
+		boolean loginTrueFalse = loginService.authenticateAdministrator(administrator.getUserId(), administrator.getPassword());
+
+		if(loginTrueFalse) {
+			model.addAttribute("administrator", administrator);
+			return "loginsuccessfuladministrator";
+		}
+		else {
+			return "loginadministrator";
+			}
+		}
 }
--- laps/src/main/java/sg/edu/nus/demo/service/LoginService.java
+++ laps/src/main/java/sg/edu/nus/demo/service/LoginService.java
@@ -1,11 +1,19 @@
 package sg.edu.nus.demo.service;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
+import sg.edu.nus.demo.exception.UsernameNotFoundException;
+import sg.edu.nus.demo.model.Administrator;
 import sg.edu.nus.demo.model.Employee;
+import sg.edu.nus.demo.model.Manager;
+import sg.edu.nus.demo.repo.AdministratorRepository;
 import sg.edu.nus.demo.repo.EmployeeRepository;
+import sg.edu.nus.demo.repo.ManagerRepository;
 
 @Service
 public class LoginService {
@@ -13,19 +21,124 @@ public class LoginService {
 	@Resource
 	private EmployeeRepository employeeRepository;
 	
+	@Resource
+	private ManagerRepository managerRepository;
+	
+	@Resource
+	private AdministratorRepository administratorRepository;
+	
 	public boolean authenticateEmployee(String username, String password) {
 		
 		Employee employee = employeeRepository.findEmployeeByUserId(username);
-		System.out.println(username);
-		System.out.println(password);
-		if(password.compareTo(employee.getPassword()) == 0) {
-			
-			System.out.println(employee.getPassword());
-			return true;
+		boolean authenticated = false;
+		try {
+			// Employee found in database
+			if(employee != null) {
+				// Hashed password using MD5
+				String hashedPw = hashPassword(password);
+				//compare password with employee password in database, return true if same, false if different
+				if(hashedPw.compareTo(employee.getPassword()) == 0) {	
+					authenticated = true;
+				}
+			}
+			else {
+				// Employee not found in database
+				throw new UsernameNotFoundException();
+			}
 		}
-
-    	//if (employee == null) throw new UsernameNotFoundException(username);
-        //compare password from form with employee password in database, return true if same, false if different
-        return false;
+		catch (UsernameNotFoundException e) {				
+			e.printStackTrace();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return authenticated;
+	}
+	
+	public boolean authenticateManager(String username, String password) {
+		
+		Manager manager = managerRepository.findManagerByUserId(username);
+		boolean authenticated = false;
+		try {
+			// Manager found in database
+			if(manager != null) {
+				// Hashed password using MD5
+				String hashedPw = hashPassword(password);
+				//compare password with manager password in database, return true if same, false if different
+				if(hashedPw.compareTo(manager.getPassword()) == 0) {	
+					authenticated = true;
+				}
+			}
+			else {
+				// Manager not found in database
+				throw new UsernameNotFoundException();
+			}
+		}
+		catch (UsernameNotFoundException e) {				
+			e.printStackTrace();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return authenticated;		
+	}
+	
+	public boolean authenticateAdministrator(String username, String password) {
+		
+		Administrator administrator = administratorRepository.findAdministratorByUserId(username);
+		boolean authenticated = false;
+		try {
+			// Administrator found in database
+			if(administrator != null) {
+				// Hashed password using MD5
+				String hashedPw = hashPassword(password);
+				//compare password with employee password in database, return true if same, false if different
+				if(hashedPw.compareTo(administrator.getPassword()) == 0) {	
+					authenticated = true;
+				}
+			}
+			else {
+				// Administrator not found in database
+				throw new UsernameNotFoundException();
+			}
+		}
+		catch (UsernameNotFoundException e) {				
+			e.printStackTrace();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return authenticated;
+	}
+	
+	public String hashPassword(String original) {
+		
+		String generatedPassword = null;
+		try {
+	        // Create MessageDigest instance for MD5
+	        MessageDigest md = MessageDigest.getInstance("MD5");
+	        //Add password bytes to digest
+	        md.update(original.getBytes());
+	        //Get the hash's bytes
+	        byte[] bytes = md.digest();
+	        //This bytes[] has bytes in decimal format;
+	        //Convert it to hexadecimal format
+	        StringBuilder sb = new StringBuilder();
+	        for(int i=0; i< bytes.length ;i++)
+	        {
+	            sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+	        }
+	        //Get complete hashed password in hex format
+	        generatedPassword = sb.toString();
+	    }
+	    catch (NoSuchAlgorithmException e)
+	    {
+	        e.printStackTrace();
+	    }
+	    
+	    return generatedPassword;
 	}
 }
