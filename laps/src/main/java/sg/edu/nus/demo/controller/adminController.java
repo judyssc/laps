0fed80c9e18017ba9a7806f8ae5f02673884ad99
LeeChenghao <lee.chenghao@gmail.com>
2019-05-24 15:07:49 +0800

    changed list_holidays method

--- laps/src/main/java/sg/edu/nus/demo/controller/adminController.java
+++ laps/src/main/java/sg/edu/nus/demo/controller/adminController.java
@@ -2,20 +2,26 @@ package sg.edu.nus.demo.controller;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import sg.edu.nus.demo.model.Calender;
 import sg.edu.nus.demo.model.Employee;
 import sg.edu.nus.demo.model.LeaveType;
+import sg.edu.nus.demo.model.PagerModel;
 import sg.edu.nus.demo.repo.CalenderRepository;
 import sg.edu.nus.demo.repo.EmployeeRepository;
 import sg.edu.nus.demo.repo.LeaveTypesRepository;
@@ -51,11 +57,26 @@ public class adminController {
 	
 	
 	@GetMapping("/admin/list_holidays")
-	public String listholiday(Model model) {
-		model.addAttribute("holidays",calRepo.findAll());
-		System.out.println(calRepo.findAll());
+	public ModelAndView listholiday(@RequestParam("pageSize") Optional<Integer> pageSize,@RequestParam("page") Optional<Integer> page) {
 		
-		return "listholiday";
+		ModelAndView modelAndView = new ModelAndView("listholiday");
+		int BUTTONS_TO_SHOW = 3;
+		int INITIAL_PAGE = 0;
+		int INITIAL_PAGE_SIZE = 10;
+		int[] PAGE_SIZES = { 5, 10};
+		int evalPageSize = pageSize.orElse(INITIAL_PAGE_SIZE);
+		int evalPage = (page.orElse(0) < 1) ? INITIAL_PAGE : page.get() - 1;
+		
+		Page<Calender> clientlist = calRepo.findAll(new PageRequest(evalPage, evalPageSize));
+		PagerModel pager = new PagerModel(clientlist.getTotalPages(), clientlist.getNumber(), BUTTONS_TO_SHOW);
+	
+		modelAndView.addObject("clientlist", clientlist);
+		modelAndView.addObject("selectedPageSize", evalPageSize);
+		modelAndView.addObject("pageSizes", PAGE_SIZES);
+		modelAndView.addObject("pager", pager);
+
+	    return modelAndView;
+
 	}
 	
 	@GetMapping("/admin/add_holidays")
