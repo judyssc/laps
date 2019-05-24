80b5244fb5f2fba44b9ec44121cdef05fbacdb2e
kishorebalaga <49152042+kishorebalaga@users.noreply.github.com>
2019-05-24 10:27:07 +0800

    admin updates

--- laps/src/main/java/sg/edu/nus/demo/controller/adminController.java
+++ laps/src/main/java/sg/edu/nus/demo/controller/adminController.java
@@ -1,8 +1,11 @@
 package sg.edu.nus.demo.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -134,12 +137,15 @@ public class adminController {
 			emp.setAnnualLeaveBalance(14);
 			empRepo.save(emp);
 		}
+		
 		return "redirect:/showemp";
 		
 	}
 	@PostMapping("/saveemp") 
-	public String saveemployee(Employee emp) {
-		
+	public String saveemployee(@Valid Employee emp,BindingResult bindingresult) {
+		if(bindingresult.hasErrors()) {
+			return "createEmp";
+		}
 		empRepo.save(emp);
 		return "redirect:/showemp";
 	}
